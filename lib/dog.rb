68cc99aecdf1c33d03520abbb3d37048d638b3e6
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-09-12 03:35:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+#require 'pry'
+
+class Dog
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def self.print_all
+    @@all.map do |dog|
+      puts dog.name
+    end
+  end
+  
+  def self.save
+    @@all << self
+  end
+end
